362907f94ddfd983af265a689dc176a0b770f551
Your Name <mahmoodyousaf975974@gmail.com>
2021-07-15 11:43:20 +0500

    Login and signup is completed with integration with firebase plus locale

--- app/src/main/java/com/example/fruit_sabzi_mandi/Intro.java
+++ app/src/main/java/com/example/fruit_sabzi_mandi/Intro.java
@@ -8,17 +8,45 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Intro extends AppCompatActivity {
-    Button btnGetStarted;
+    Button btnUrdu;
+    Button btnEnglish;
+    Button commissionDashboard;
+    Button farmerDashboard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-        btnGetStarted = (Button) findViewById(R.id.commissionDashboard);
-        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+        commissionDashboard = (Button) findViewById(R.id.commissionDashboard);
+        farmerDashboard = (Button) findViewById(R.id.farmerDashboard);
+        btnUrdu = (Button) findViewById(R.id.Urdu);
+        btnEnglish = (Button) findViewById(R.id.English);
+        LanguageManager languageManager=new LanguageManager(this);
+        commissionDashboard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //startActivity(new Intent(com.example.quizapplication.Intro.this,.class));
             }
         });
+        btnUrdu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                languageManager.updateResourse("ur");
+                recreate();
+            }
+        });
+        btnEnglish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                languageManager.updateResourse("en");
+                recreate();
+            }
+        });
+        commissionDashboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        });
+
     }
 }
