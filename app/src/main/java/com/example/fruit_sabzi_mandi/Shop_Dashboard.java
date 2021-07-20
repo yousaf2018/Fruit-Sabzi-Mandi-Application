8e58de32b89a64e6d0bc21f493a3582d57cea8c8
Your Name <mahmoodyousaf975974@gmail.com>
2021-07-20 15:34:24 +0500

    Authentication is completed

---
+++ app/src/main/java/com/example/fruit_sabzi_mandi/Shop_Dashboard.java
@@ -0,0 +1,14 @@
+package com.example.fruit_sabzi_mandi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Shop_Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop__dashboard);
+    }
+}
