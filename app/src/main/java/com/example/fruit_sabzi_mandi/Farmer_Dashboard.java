479ec40ab531d6dd71dbea66c282c4db568571b9
Your Name <mahmoodyousaf975974@gmail.com>
2021-08-10 17:56:59 +0500

    Project is completed

---
+++ app/src/main/java/com/example/fruit_sabzi_mandi/Farmer_Dashboard.java
@@ -0,0 +1,14 @@
+package com.example.fruit_sabzi_mandi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Farmer_Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farmer__dashboard);
+    }
+}
---
+++ app/src/main/java/com/example/fruit_sabzi_mandi/adapters/comissionShopsContactsAdapter.java
@@ -0,0 +1,4 @@
+package com.example.fruit_sabzi_mandi.adapters;
+
+public class commissionShopsContactsAdapter {
+}
